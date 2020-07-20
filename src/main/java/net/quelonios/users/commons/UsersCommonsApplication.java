cb0174b87d252ba24d476e790b1adec1265b9e29
gerardoharias <gerardoharias@gmail.com>
2020-07-19 21:11:45 -0500

    versionando proyecto common users

---
+++ src/main/java/net/quelonios/users/commons/UsersCommonsApplication.java
@@ -0,0 +1,12 @@
+package net.quelonios.users.commons;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class UsersCommonsApplication {
+
+
+}
